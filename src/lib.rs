23300eae543295ff34dc764c869050a7add32820
Tibor Csóka <csokat@gmail.com>
2025-12-10 10:10:07 +0100

    chore(deps): public export of Address type

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,9 @@ pub mod safe_module;
 #[allow(clippy::too_many_arguments)]
 pub mod utils;
 pub mod win_prob;
+
+pub mod exports {
+    pub use hopr_bindings::exports::alloy;
+}
+
+pub use hopr_bindings::exports::alloy::primitives::Address;
